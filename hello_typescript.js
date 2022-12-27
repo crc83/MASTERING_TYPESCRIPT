9236e6bad3b628cacecd714c36b9a240c24c0be3
Sergiy Beley <sergiy.beley@gmail.com>
2022-12-27 14:26:38 +0200

    initial

---
+++ hello_typescript.js
@@ -0,0 +1,17 @@
+"use strict";
+var myBoolean = true;
+var myNumber = 1234;
+var myStringArray = [`first`, `second`, `third`];
+var version = `es2016`;
+myBoolean = myNumber === 456;
+myStringArray = [myNumber.toString(), `5678`];
+myNumber = myStringArray.length;
+console.log(`Hello ${version} Typescript`);
+console.log(`myBoolean=${myBoolean}`);
+console.log(`myStringArray=${myStringArray}`);
+console.log(`myNumber=${myNumber}`);
+var inferredString = "this is string";
+var inferredNumber = 42;
+inferredNumber = inferredString;
+console.log(`inferredString=${inferredString}`);
+console.log(`inferredNumber=${inferredNumber}`);
