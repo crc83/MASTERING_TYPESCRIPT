6e3bbefd1519fd5c036080b03d2e749d4e8f8690
Sergiy Beley <sergiy.beley@gmail.com>
2023-01-21 23:07:12 +0200

    nulls etc

--- ch02/primitive_types.js
+++ ch02/primitive_types.js
@@ -1,7 +1,9 @@
 "use strict";
-var array = ["10", "20", "40"];
+var _a;
+var array = ["10", "20", "40", null];
 delete array[1];
 for (var i = 0; i < array.length; i++) {
     console.log("array[".concat(i, "] = ").concat(array[i]));
+    console.log("array[".concat(i, "] = ").concat(((_a = array[i]) !== null && _a !== void 0 ? _a : "42")));
 }
 //# sourceMappingURL=primitive_types.js.map
--- ch02/primitive_types.ts
+++ ch02/primitive_types.ts
@@ -1,7 +1,8 @@
-let array = ["10", "20", "40"];
+let array = ["10", "20", "40", null];
 
 delete array[1];
 
 for(let i = 0; i < array.length; i++) {
   console.log(`array[${i}] = ${array[i]}`);
+  console.log(`array[${i}] = ${( array[i] ?? "42")}`);
 }
